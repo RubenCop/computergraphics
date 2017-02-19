a819e3fba29e6a70c86e382e4a9901072912aa72
Jan Willem <jwdwit@gmail.com>
2017-02-19 20:12:51 +0100

    sphere netjes gemaakt

--- Week2/Raytracer/sphere.h
+++ Week2/Raytracer/sphere.h
@@ -1,19 +1,3 @@
-//
-//  Framework for a raytracer
-//  File: sphere.h
-//
-//  Created for the Computer Science course "Introduction Computer Graphics"
-//  taught at the University of Groningen by Tobias Isenberg.
-//
-//  Authors:
-//    Maarten Everts
-//    Jasper van de Gronde
-//
-//  This framework is inspired by and uses code of the raytracer framework of 
-//  Bert Freudenberg that can be found at
-//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
-//
-
 #ifndef SPHERE_H_115209AE
 #define SPHERE_H_115209AE
 
